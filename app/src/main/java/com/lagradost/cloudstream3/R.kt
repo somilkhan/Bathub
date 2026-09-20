65eeb5c1d46f4b3a4729358d0361b7164216b9db
Zen <81418397+somilkhan@users.noreply.github.com>
2026-09-20 23:20:06 +0530

    fix(player): support non-final Android resource ids

--- app/src/main/java/com/lagradost/cloudstream3/R.kt
+++ app/src/main/java/com/lagradost/cloudstream3/R.kt
@@ -9,18 +9,18 @@ package com.lagradost.cloudstream3
  */
 object R {
     object string {
-        const val autoplay_next_key = com.hikari.app.R.string.cs_autoplay_next_key
-        const val go_back = com.hikari.app.R.string.cs_go_back
-        const val ok = com.hikari.app.R.string.cs_ok
-        const val play_torrent_button = com.hikari.app.R.string.cs_play_torrent_button
-        const val prefer_media_type_key = com.hikari.app.R.string.cs_prefer_media_type_key
-        const val quality_pref_key = com.hikari.app.R.string.cs_quality_pref_key
-        const val quality_pref_mobile_data_key = com.hikari.app.R.string.cs_quality_pref_mobile_data_key
-        const val software_decoding_key = com.hikari.app.R.string.cs_software_decoding_key
-        const val torrent_info = com.hikari.app.R.string.cs_torrent_info
-        const val torrent_not_accepted = com.hikari.app.R.string.cs_torrent_not_accepted
-        const val torrent_preferred_media = com.hikari.app.R.string.cs_torrent_preferred_media
-        const val subtitles_encoding_key = com.hikari.app.R.string.cs_subtitles_encoding_key
-        const val preview_seekbar_key = com.hikari.app.R.string.cs_preview_seekbar_key
+        @JvmField val autoplay_next_key = com.hikari.app.R.string.cs_autoplay_next_key
+        @JvmField val go_back = com.hikari.app.R.string.cs_go_back
+        @JvmField val ok = com.hikari.app.R.string.cs_ok
+        @JvmField val play_torrent_button = com.hikari.app.R.string.cs_play_torrent_button
+        @JvmField val prefer_media_type_key = com.hikari.app.R.string.cs_prefer_media_type_key
+        @JvmField val quality_pref_key = com.hikari.app.R.string.cs_quality_pref_key
+        @JvmField val quality_pref_mobile_data_key = com.hikari.app.R.string.cs_quality_pref_mobile_data_key
+        @JvmField val software_decoding_key = com.hikari.app.R.string.cs_software_decoding_key
+        @JvmField val torrent_info = com.hikari.app.R.string.cs_torrent_info
+        @JvmField val torrent_not_accepted = com.hikari.app.R.string.cs_torrent_not_accepted
+        @JvmField val torrent_preferred_media = com.hikari.app.R.string.cs_torrent_preferred_media
+        @JvmField val subtitles_encoding_key = com.hikari.app.R.string.cs_subtitles_encoding_key
+        @JvmField val preview_seekbar_key = com.hikari.app.R.string.cs_preview_seekbar_key
     }
 }
